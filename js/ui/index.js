6ff7fc1ea6e848df371ddb2a7cc47140f730f2c2
joelperalta <joel.jppz@gmail.com>
2026-02-10 15:03:09 -0600

    feat(form): add task creation form  style and JS Logic

--- js/ui/index.js
+++ js/ui/index.js
@@ -1,2 +1,36 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+
+
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskFormOverlay = document.getElementById('task-form-overlay');
+
+
+addTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    taskFormOverlay.style.display = 'flex';
+
+});
+
+const sidebarEl = document.getElementById('sidebar');
+const sidebar = new bootstrap.Offcanvas(sidebarEl);
+
+sidebarEl.addEventListener('click', (e) => {
+    if (taskFormOverlay.style.display === "flex") {
+        sidebar.hide();
+    }
+});
+
+const cancelBtn = document.getElementById('cancel-btn');
+cancelBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    taskFormOverlay.style.display = 'none';
+});
+
+
+
+
+
+
+});
 
