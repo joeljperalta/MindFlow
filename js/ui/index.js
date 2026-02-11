9eec17db52aaaf84558c5dfbf5f9bf5cc588297a
joelperalta <joel.jppz@gmail.com>
2026-02-11 11:45:37 -0600

    feat(card): display task in card

--- js/ui/index.js
+++ js/ui/index.js
@@ -30,6 +30,46 @@ cancelBtn.addEventListener('click', (e) => {
 
 
 
+const title = document.getElementById('taskInput');
+const priority = document.getElementById('taskPriority');
+const dueDate = document.getElementById('taskDeadline');
+
+
+const summitTaskBtn = document.getElementById('Summit-task');
+const cardContainer = document.getElementById('Card-Container');
+
+summitTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let badgeClass;
+    if (priority.value === 'High') badgeClass = 'bg-danger';
+    else if(priority.value === 'Medium') badgeClass = 'bg-warning text-dark';
+    else badgeClass = 'bg-success';
+
+    
+    const cardHtml = `
+    <div class="card mb-3 shadow-sm">
+        <div class="card-body">
+            <h5 class="card-title">${title.value}</h5>
+            <p class="card-text">
+            Priority: <span class="badge ${badgeClass}">${priority.value}</span></p>
+
+            <p class="card-text">Due: <strong>${dueDate.value}</strong></p>
+            <button class="btn btn-sm btn-outline-secondary me-2 complete-btn">Complete</button>
+            <button class="btn btn-sm btn-outline-danger delete-btn">Delete</button>
+        </div>
+    </div>
+    `;
+
+   cardContainer.append(document.createRange().createContextualFragment(cardHtml));
+
+    
+    taskFormOverlay.style.display = 'none';
+    title.value = '';
+    priority.value = '';
+    dueDate.value = '';
+
+});
 
 
 });
